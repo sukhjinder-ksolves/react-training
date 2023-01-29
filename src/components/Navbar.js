3121bc17a127c8a9a91fcdf41084fe2aeaf87245
Sukhjinder Singh <sukhjinder.singh@ksolves.com>
2023-01-29 23:24:58 +0530

    used Link from next

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <a className="navbar-brand" href="/">React Practice</a>
+                <Link className="navbar-brand" href="/">React Practice</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
